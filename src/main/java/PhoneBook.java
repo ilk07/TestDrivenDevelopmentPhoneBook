e8701113bb4fcc379c0b13bc89978fd4306ec9b7
ilk07 <ilk07@inbox.ru>
2023-02-18 17:02:25 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,8 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
-    public int add(String name, String phone){
+    private final Map<String, String> contacts = new HashMap<>();
+
+    public int add(String name, String phone) {
+
         // Метод должен возвращать количество контактов после добавления,
         // при этом гарантируется, что не будут добавляться повторяющиеся имена;
-        return 0;
+        if (name != null) {
+            if (!name.trim().equals("")) {
+                if (!contacts.containsKey(name)) {
+                    contacts.put(name, phone);
+                }
+            }
+        }
+
+        return contacts.size();
     }
 }
