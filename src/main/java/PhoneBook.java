4319834d88bc0ac3e9913701ff1f6cef0937d2d2
ilk07 <ilk07@inbox.ru>
2023-02-18 19:18:53 +0300

    findByNumber implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -21,6 +21,23 @@ public class PhoneBook {
     }
 
     public String findByNumber(String phone){
-        return null;
+        //найти имя по номеру без полного перебора
+
+        if (phone == null){
+            return "Номер не существует";
+        }
+
+        if(phone.trim().equals("")){
+            return "Номер не указан";
+        }
+
+        if(contacts.isEmpty()){
+            return "Список контактов пуст";
+        } else {
+            return contacts.entrySet().stream()
+                    .filter(entry -> phone.equals(entry.getValue()))
+                    .findFirst().map(Map.Entry::getKey)
+                    .orElse("Контакт не найден");
+        }
     }
 }
